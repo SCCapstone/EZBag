b0bfa2fe69928f3f486b9f0c17e891d8726fd231
brendan <brendanreidy16@gmail.com>
2020-10-25 19:37:53 -0400

    local changes

--- EZBagWebapp/src/main/java/debug/main.java
+++ EZBagWebapp/src/main/java/debug/main.java
@@ -26,51 +26,50 @@ public class main {
         DatabaseService.database = mongo;
         ReceiptService.database = mongo;
 
-//        System.out.println("Simple MongoDB Connector");
-//        MongoClient informalDB = new MongoClient("localhost", 27017);
-//        MongoCredential credential;
-//        credential = MongoCredential.createCredential("blake", "myDB", "password".toCharArray());
-//        System.out.println("Connected to database");
-//        MongoDatabase database = informalDB.getDatabase("myDB");
-//        MongoCollection<Document> collection = database.getCollection("product");
+        System.out.println("Simple MongoDB Connector");
+        MongoClient informalDB = new MongoClient("localhost", 27017);
+        MongoCredential credential;
+        credential = MongoCredential.createCredential("blake", "myDB", "password".toCharArray());
+        System.out.println("Connected to database");
+        MongoDatabase database = informalDB.getDatabase("myDB");
+        MongoCollection<Document> collection = database.getCollection("product");
 
 
-//        List<Document> products = new ArrayList<Document>();
-//        products.add(Document.parse("{\"upc\" : \"670433093100\", \"ean\" : \"0670433093100\", \"name\" : \"Ground Chicken\", \"price\" : 8.99, \"description\" : \"1 lb of Vegetable Fed Ground Chicken. No Antibiotics.\" }"));
-//        products.add(Document.parse("{\"upc\" : \"024600010436\", \"ean\" : \"0024600010436\", \"name\" : \"Iodized Salt\", \"price\" : 3.99, \"description\" : \"100% pure salt.\" }"));
-//        products.add(Document.parse("{\"upc\" : \"226270000001\", \"ean\" : \"0226270000001\", \"name\" : \"Whole Muscle Beef\", \"price\" : 10.99, \"description\" : \"Beef Hind Shank.\" }"));
-//        products.add(Document.parse("{\"upc\" : \"810180026178\", \"ean\" : \"0810180026178\", \"name\" : \"Melatonin\", \"price\" : 10.99, \"description\" : \"Makes you fall asleep fast.\" }"));
-//        products.add(Document.parse("{\"upc\" : \"696554738959\", \"ean\" : \"0696554738959\", \"name\" : \"Sword Floss\", \"price\" : 5.99, \"description\" : \"Disposable Floss Picks Mint 50 Ct.\" }"));
-//        products.add(Document.parse("{\"upc\" : \"06827465\", \"ean\" : \"0696554738959\", \"name\" : \"Sword Floss\", \"price\" : 5.99, \"description\" : \"Disposable Floss Picks Mint 50 Ct.\" }"));
-//
-//        Document waterBottle = new Document("ean", "06827465")
-//                .append("name", "Nestle Pure Life Water")
-//                .append("description", "Nestle Pure Life Purified Water comes in convenient plastic, resealable bottles, for easy, on-the-go hydration.");
-//        products.add(waterBottle);
-//        Document salt = new Document("ean", "0050428427439")
-//                .append("name", "Iodized Salt")
-//                .append("description", "Iodized salt is table salt mixed with a minute amount of various salts of the element iodine. The ingestion of iodine prevents iodine deficiency.");
-//        products.add(salt);
-//
-//        collection.insertMany(products);
-//        System.out.println("Inserted starter documents");
-//
-//        StartupService.startup();
-//        System.out.println(DatabaseService.getByProductCode("810180026178"));
-//        System.out.println(DatabaseService.getByProductCode("0670433093100"));
+        List<Document> products = new ArrayList<Document>();
+        products.add(Document.parse("{\"upc\" : \"670433093100\", \"ean\" : \"0670433093100\", \"name\" : \"Ground Chicken\", \"price\" : 8.99, \"description\" : \"1 lb of Vegetable Fed Ground Chicken. No Antibiotics.\" }"));
+        products.add(Document.parse("{\"upc\" : \"024600010436\", \"ean\" : \"0024600010436\", \"name\" : \"Iodized Salt\", \"price\" : 3.99, \"description\" : \"100% pure salt.\" }"));
+        products.add(Document.parse("{\"upc\" : \"226270000001\", \"ean\" : \"0226270000001\", \"name\" : \"Whole Muscle Beef\", \"price\" : 10.99, \"description\" : \"Beef Hind Shank.\" }"));
+        products.add(Document.parse("{\"upc\" : \"810180026178\", \"ean\" : \"0810180026178\", \"name\" : \"Melatonin\", \"price\" : 10.99, \"description\" : \"Makes you fall asleep fast.\" }"));
+        products.add(Document.parse("{\"upc\" : \"696554738959\", \"ean\" : \"0696554738959\", \"name\" : \"Sword Floss\", \"price\" : 5.99, \"description\" : \"Disposable Floss Picks Mint 50 Ct.\" }"));
+        products.add(Document.parse("{\"upc\" : \"06827465\", \"ean\" : \"0696554738959\", \"name\" : \"Sword Floss\", \"price\" : 5.99, \"description\" : \"Disposable Floss Picks Mint 50 Ct.\" }"));
+
+        Document waterBottle = new Document("ean", "06827465")
+                .append("name", "Nestle Pure Life Water").append("description", "Nestle Pure Life Purified Water comes in convenient plastic, resealable bottles, for easy, on-the-go hydration.");
+        products.add(waterBottle);
+        Document salt = new Document("ean", "0050428427439")
+                .append("name", "Iodized Salt")
+                .append("description", "Iodized salt is table salt mixed with a minute amount of various salts of the element iodine. The ingestion of iodine prevents iodine deficiency.");
+        products.add(salt);
+
+        collection.insertMany(products);
+        System.out.println("Inserted starter documents");
+
+        StartupService.startup();
+        System.out.println(DatabaseService.getByProductCode("810180026178"));
+        System.out.println(DatabaseService.getByProductCode("0670433093100"));
 
 
 
-//        // Testing customer info insertion
-//        Document customerInfo = new Document("email", "blakeedwards823@gmail.com").append("phone", "8603337654");
-//        String resp = DatabaseService.insertInfo(customerInfo);
-//        System.out.println(resp);
-//        Document customerInfo2 = new Document("email", "mikercutro@gmail.com");
-//        String resp2 = DatabaseService.insertInfo(customerInfo2);
-//        System.out.println(resp2);
-//        Document customerInfo3 = new Document("phone", "8603333333");
-//        String resp3 = DatabaseService.insertInfo(customerInfo3);
-//        System.out.println(resp3);
+        // Testing customer info insertion
+        Document customerInfo = new Document("email", "blakeedwards823@gmail.com").append("phone", "8603337654");
+        String resp = DatabaseService.insertInfo(customerInfo);
+        System.out.println(resp);
+        Document customerInfo2 = new Document("email", "mikercutro@gmail.com");
+        String resp2 = DatabaseService.insertInfo(customerInfo2);
+        System.out.println(resp2);
+        Document customerInfo3 = new Document("phone", "8603333333");
+        String resp3 = DatabaseService.insertInfo(customerInfo3);
+        System.out.println(resp3);
 
 //        // TODO insert customer scanned item event
 //        Document customerScannedItemEventObject = new Document("time", System.currentTimeMillis())
