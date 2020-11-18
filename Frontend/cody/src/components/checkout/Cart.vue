9abf65e0100eaad0610cdafbbccb04a0d14eed8e
Cody Shearer <cjshearer@live.com>
2020-11-17 21:30:43 -0500

    added frontend function for incrementing/decrementing product quantity changed layout of list and placement of delete-item button - minus sign button changes to a delete button when product quantity is 1

--- Frontend/cody/src/components/checkout/Cart.vue
+++ Frontend/cody/src/components/checkout/Cart.vue
@@ -2,19 +2,41 @@
   <div>
     <v-container class="products">
       <v-row
-      v-for="product in getCart"
-      v-bind:key="product.barcode"
+        v-for="product in getCart"
+        v-bind:key="product.barcode"
       >
-        <v-col class="flex-grow-1 flex-shrink-0">
-          {{ product.name }}
+        <v-col>
+          <v-card>
+            <v-card-title>{{ product.name }}</v-card-title>
+            <v-card-actions>
+              <span class="subheading ml-2"> ${{ product.price }}</span>
+              <v-spacer></v-spacer>
+              <!-- Remove product button - conditionally rendered-->
+              <v-btn 
+                @click="removeProduct({barcode:product.barcode})"
+                v-show="product.quantity === 1"
+                icon
+              >
+              <!-- Decrement product quantity - conditionally rendered on product.quantity-->
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+              <v-btn 
+                @click="adjustProductQuantity({barcode:product.barcode,amount:-1})"
+                v-show="product.quantity !== 1" 
+                icon
+              >
+                <v-icon>mdi-minus</v-icon>
+              </v-btn>
+              <!-- Item quantity -->
+              <!-- TODO: make this a button, which can be clicked to edit the quantity directly-->
+              <span class="subheading mr-2 ml-2"> {{ product.quantity }}</span>
+              <!-- Increment product quantity -->
+              <v-btn @click="adjustProductQuantity({barcode:product.barcode,amount:1})" icon>
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
-        <v-col class="flex-grow-0 flex-shrink-1">
-          ${{ product.price }}
-        </v-col>
-        <v-col class="flex-grow-0 flex-shrink-1">
-          <v-icon @click="removeProduct(product.barcode)">mdi-delete</v-icon>
-        </v-col>
-        <!-- TODO: add increment/decrement (adjust) quantity buttons -->
       </v-row>
     </v-container>
   </div>
@@ -37,6 +59,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+/*
+  Fix for word-wrapping in title. To be fixed in Vuetify 3.
+  https://github.com/vuetifyjs/vuetify/issues/9130#issuecomment-542534966
+*/
+.v-card__text, .v-card__title {
+  word-break: normal;
+}
 
 </style>
