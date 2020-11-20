29bc5da3d78bf21e336ffe23434441be8db27d91
Cody Shearer <cjshearer@live.com>
2020-11-20 14:10:51 -0500

    separated product card from cart for reusability

--- Frontend/cody/src/components/checkout/Cart.vue
+++ Frontend/cody/src/components/checkout/Cart.vue
@@ -6,36 +6,14 @@
         v-bind:key="product.barcode"
       >
         <v-col>
-          <v-card>
-            <v-card-title>{{ product.name }}</v-card-title>
-            <v-card-actions>
-              <span class="subheading ml-2"> ${{ product.price }}</span>
-              <v-spacer></v-spacer>
-              <!-- Remove product button - conditionally rendered-->
-              <v-btn 
-                @click="removeProduct({barcode:product.barcode})"
-                v-show="product.quantity === 1"
-                icon
-              >
-              <!-- Decrement product quantity - conditionally rendered on product.quantity-->
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn 
-                @click="adjustProductQuantity({barcode:product.barcode,amount:-1})"
-                v-show="product.quantity !== 1" 
-                icon
-              >
-                <v-icon>mdi-minus</v-icon>
-              </v-btn>
-              <!-- Item quantity -->
-              <!-- TODO: make this a button, which can be clicked to edit the quantity directly-->
-              <span class="subheading mr-2 ml-2"> {{ product.quantity }}</span>
-              <!-- Increment product quantity -->
-              <v-btn @click="adjustProductQuantity({barcode:product.barcode,amount:1})" icon>
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+          <Product 
+          v-bind:barcode=product.barcode
+          v-bind:name=product.name
+          v-bind:price=product.price
+          v-bind:description=product.description
+          v-bind:businessID=product.businessID
+          v-bind:quantity=product.quantity
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -44,29 +22,14 @@
 
 <script>
 // https://vuex.vuejs.org/guide/actions.html#dispatching-actions-in-components
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+import Product from '@/components/checkout/Product'
 
 export default {
   name:"Cart",
-  methods: {
-    ...mapActions(["addProduct",
-                   "removeProduct",
-                   "setProductQuantity",
-                   "adjustProductQuantity"
-                  ])
+  components: {
+    Product
   },
   computed: mapGetters(['getCart'])
 }
 </script>
-
-<style scoped>
-
-/*
-  Fix for word-wrapping in title. To be fixed in Vuetify 3.
-  https://github.com/vuetifyjs/vuetify/issues/9130#issuecomment-542534966
-*/
-.v-card__text, .v-card__title {
-  word-break: normal;
-}
-
-</style>
---
+++ Frontend/cody/src/components/checkout/Product.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <v-card-title>{{ name }}</v-card-title>
+    <v-card-actions>
+      <span class="subheading ml-2"> ${{ price }}</span>
+      <v-spacer></v-spacer>
+      <!-- Remove product button - conditionally rendered-->
+      <v-btn 
+        @click="removeProduct({barcode})"
+        v-show="quantity === 1"
+        icon
+      >
+      <!-- Decrement product quantity - conditionally rendered on product.quantity-->
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+      <v-btn 
+        @click="adjustProductQuantity({barcode:barcode,amount:-1})"
+        v-show="quantity !== 1" 
+        icon
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <!-- Item quantity -->
+      <!-- TODO: make this a button, which can be clicked to edit the quantity directly-->
+      <span class="subheading mr-2 ml-2"> {{ quantity }}</span>
+      <!-- Increment product quantity -->
+      <v-btn @click="adjustProductQuantity({barcode:barcode,amount:1})" icon>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: ['barcode',
+          'name',
+          'price',
+          'description',
+          'businessID',
+          'quantity'],
+  methods: {
+  ...mapActions([ "removeProduct",
+                  "setProductQuantity",
+                  "adjustProductQuantity"
+                ])
+  },
+}
+</script>
+
+<style scoped>
+
+/*
+  Fix for word-wrapping in title. To be fixed in Vuetify 3.
+  https://github.com/vuetifyjs/vuetify/issues/9130#issuecomment-542534966
+*/
+.v-card__text, .v-card__title {
+  word-break: normal;
+}
+
+</style>
