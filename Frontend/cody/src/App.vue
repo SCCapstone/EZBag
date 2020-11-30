35966e7a89dd0aba21a8bf4bf2d1b48269e89c80
Cody Shearer <cjshearer@live.com>
2020-11-30 17:33:25 -0500

    added elevation feature for navbar

--- Frontend/cody/src/App.vue
+++ Frontend/cody/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app elevate-on-scroll color="primary" dark>
       <v-tabs fixed-tabs>
         <v-tab to="/">Home</v-tab>
         <v-tab to="/cart">Cart</v-tab>
