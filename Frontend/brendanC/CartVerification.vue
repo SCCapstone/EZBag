1f77cc14c38a9ae8b67cf925ed8cbcd750df928e
Brendan Curran <bcurran@email.sc.edu>
2020-12-03 17:17:39 -0500

    cartverification view

---
+++ Frontend/brendanC/CartVerification.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="CartVerification">
+    <ListofCarts />
+    <template>
+    <v-row
+        v-for="carts in getListofCarts"
+        v-bind:key="Cart.CartList"
+    >
+    <v-column
+        <Cart = v-bind:CartList=Cart.CartList \:>
+    >
+    </v-column>
+    </v-row>
+    </template>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import Product from '@/components/checkout/Product'
+
+export default {
+  name: 'CartVerification',
+  components: {
+      ListofCarts
+    },
+  computed: mapGetters(['getListofCarts']),
+  data() {
+    return {
+        ListofCarts
+    };
+  }
+};
+</script>
