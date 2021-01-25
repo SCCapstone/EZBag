9c5c496e99434794ec46c35683630ab1854073d4
Cody Shearer <cjshearer@live.com>
2021-01-25 15:05:47 -0500

    uses environment variable for api root and some changes to scanner function (for sure still buggy)

--- Frontend/customer/src/components/scan/Scanner.vue
+++ Frontend/customer/src/components/scan/Scanner.vue
@@ -7,23 +7,23 @@
       v-model="show_scanned_product"
       inset
     >
-    <v-sheet>
-      <v-card>
-        <Product 
-        v-bind:barcode="scanned_product_barcode"
-        v-bind:elevation="0"
-        v-on:removed-product="hideScannedProductCard"
-        />
-        <v-card-actions class="justify-center">
-          <v-btn @click="cancelScannedProduct">Remove
-            <v-icon right>mdi-close</v-icon>
-          </v-btn>
-          <v-btn @click="hideScannedProductCard">Add to
-            <v-icon right>mdi-cart</v-icon>
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-sheet>
+      <v-sheet>
+        <v-card>
+          <Product 
+          v-bind:barcode="scanned_product_barcode"
+          v-bind:elevation="0"
+          v-on:removed-product="hideScannedProductCard"
+          />
+          <v-card-actions class="justify-center">
+            <v-btn @click="cancelScannedProduct">Remove
+              <v-icon right>mdi-close</v-icon>
+            </v-btn>
+            <v-btn @click="hideScannedProductCard">Add to
+              <v-icon right>mdi-cart</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-sheet>
     </v-bottom-sheet>
   </div>
 </template>
@@ -33,6 +33,8 @@ import { mapGetters, mapActions} from 'vuex';
 import ScanButtons from '@/components/scan/ScanButtons'
 import Product from '@/components/checkout/Product'
 import * as ScanditSDK from "scandit-sdk";
+
+let barcodePicker;
 
 import jQuery from 'jquery'
 global.jQuery = jQuery
@@ -54,30 +56,36 @@ export default {
     };
   },
   mounted () {
-    var ref = this
-    ScanditSDK.configure("AUZPWwnfFoljEUPsdi9NGuRA5C5zF7MWiH2ATlBzzRoUMTzzCyz2eIsc4K/IXEg79zR0P/MyBvLGWYeasF4Mb8dy0uW0N2YJP129zwZy2Lp0UWEdXmEzKCRdM8SaeS63XlnLFaZKvjQUeTBPYgcylbkVWWH6KcrqvBrisCWjchbaBjIkY1Jg04dgfp+bv0AEn1+Zx2hUKE94qzXxHXGos8w4n7JOgigJVYbC4xgWS6g/3WmXebi22sBQh3gxkQqGEsfKC5XLPPPXS4ZM3GQR8Yih2iTOe0F88gkbwcQj86EMURTe6/3o7iobP13CS7DkIBJX2kq/i/fXbPrr0Hey46yRJM7z5TaZeMbQV9j+fe9TNBPlsNdZ1HOVwjKgYqxBSqNBuBJ8K7gGz+yaV8vkV+xVGroIWNfolxt5BQeRrX+aqFc65NnfSuYyMds01a5M0Mj8cXwWwBJCifFpLPE02KrZkOmJ/gYC+un1qGbjkn1y0sbXdrhun5utExw6atXKvfQ04WOI/jwTSmdzo6RtMqdFUit5vhls8x1aPkPUWSVmpOQC2ot+CifFfW19ltPhklmNryRAot8Nso3AMkzbMW9h1sMb/gFyf8eIP4Zcs5BsnWNDVKGJrSyznv5JlJpQQiGdyb6QVa5F0sqt4KUT9uiquaMDVjxeDusuWPgv3pooHDre/Rf34ffbtEd7uWcuF7wLqZ6BbnNSA6BMv45Sh3tZ4v1arG7NGNSZUZKg23qUiPoP+XvcmbAtHcN4vfZCaywUj60pv916l4/0DXrlqRFnA5aSx/yKh4Evu4PgROYieHH2Vd67HnePg1Dq5FMZkySgfkagORlufg==", {
-      engineLocation: "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build/",
-    })
-      .then(() => {
-        return ScanditSDK.BarcodePicker.create(document.getElementById("scandit-barcode-picker"), {
-          // enable some common symbologies
-          videoFit: ScanditSDK.BarcodePicker.ObjectFit.COVER,
-          scanSettings: new ScanditSDK.ScanSettings({ enabledSymbologies: ["ean8", "ean13", "upca", "upce"] }),
-        });
+    // https://github.com/Scandit/barcodescanner-sdk-for-web-samples/blob/master/vanilla-samples/proof-of-delivery/index.html
+    ScanditSDK.configure("AcN/DBgYOL7AKEDqcipZkFZAlsxfPT8wWnSu1HF/oAPwSPPNtlDJw/RzfunnSuo0fVUK1YUao28BYQ+vHUE86/8JaHQET5B1h3rJ8rRk8UcRdXx5pCBhz5QF5Fn1ODWosRhAJzNM0hxpXmSD2OjbpljflTYHVmzyCcIQTMsEAasUgE2cMu1Odx+TCUcZVIBvndjZdI473l4AEjl4VSZlzi/Iw103XcaWRj85gCTtFGRhw01sd356F8eoxvLGfAqS71GGiICahkk3GGm685hpup1myHzM7KB51cWksy2VCSmSLVHgprAlsDDihIaLEFBiJXpP1c0/6cGcE0Z2pHHNlSDr3WNnvT/A1EJaKrFSgZp8jqNnSYD9sqFHnuWazvY/lzc6CWNhG8L1OQ35HpLw+XFL/PB/qOrqfFj6boVaXvqEYFiCQZNgAhmkZivdumTROV8lWuP+BVypLD2pHvbm1iDcDmkXTWQhRymYqY8WuBqEINYMaYKny3exWhIoHW6rIofsjtshAZUZB+abul36dvv2STnU16tsG9/4Edibh0hnfUk5U8Cq/w/WeWGMhAosUpNCwUh1mOPpznJBDJr/kl5FkahLuBUsh40IMpltrleFfB0+7CuUFsJTxL0zKis7Xz7xgR4AGY8WmIKBnlgOYmSGO4TojK8fAVyuVfnWmg+uXrAucsCDpRlDPvrTnAk1XoyI3NZKif2rWl7ksglaVntNX3tNeXMObUKGKlQZfCvb4VnciPUe+OxUcd8GmOW9c830fR9sjkSu/m+E18OZNlSP1uq8dWDe/kEw2ByiNFp1fSBMWQ==", {
+      engineLocation: "https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build",
       })
-      .then((barcodePicker) => {
-        // barcodePicker is ready here, show a message every time a barcode is scanned
-        barcodePicker.on("scan", (scanResult) => {
-          var barcode = scanResult.barcodes[0].data
-          if (this.show_scanned_product==false && this.scanned_product_barcode !== barcode) {
-            console.log("Scanning")
-            this.scanned_product_barcode = barcode
-            ref.onScan(barcode);
-          } else {
-            console.log("Product scanned but product card already showing!")
-          }
-        });
+        .then(() => {
+          return ScanditSDK.BarcodePicker.create(document.getElementById("scandit-barcode-picker"), {
+          videoFit: ScanditSDK.BarcodePicker.ObjectFit.COVER,
+        }).then((picker) => {
+          barcodePicker = picker;// access to barcode picker outside of ScanditSDK call
+          barcodePicker.applyScanSettings(
+            new ScanditSDK.ScanSettings({
+              enabledSymbologies: [
+                ScanditSDK.Barcode.Symbology.UPCA,
+                ScanditSDK.Barcode.Symbology.UPCE,
+                ScanditSDK.Barcode.Symbology.EAN8,
+                ScanditSDK.Barcode.Symbology.EAN13
+              ],
+              codeDuplicateFilter: -1,
+            })
+          );
+          barcodePicker
+            .on("scan", (scanResult) => {
+              barcodePicker.pauseScanning();
+              var barcode = scanResult.barcodes[0].data
+              this.onScan(barcode);
+            })
+            .on("scanError", console.error);
       });
+    })
+    .catch(console.error);
   },
   methods:{
     ...mapActions([ "removeProduct",
@@ -93,14 +101,13 @@ export default {
         this.product_loaded_from_cart = true
         // save the state of the scanned product before allowing user to make changes
         this.initial_product_quantity = product.quantity
-        this.show_scanned_product = true
       } else {
         // product not in cart
         this.product_loaded_from_cart = false
         this.getProduct(barcode, this.getCartBusinessID);
       }
-      // TODO: implement function to pause scanner while show_scanned_product = true
-      
+      this.scanned_product_barcode = barcode
+      this.show_scanned_product = true
     },
     // if user cancels adding the scanned product, we need to undo any changes they have made
     // if the product was loaded from the cart, we need to restore the product's quantity
@@ -108,22 +115,17 @@ export default {
     hideScannedProductCard() {
       this.show_scanned_product = false;
       this.scanned_product_barcode = null;
+      console.log('resume scanning', barcodePicker)
+      barcodePicker.resumeScanning();
     },
     cancelScannedProduct() {
-      if (this.product_loaded_from_cart == true) {
-        console.log('line129:',this.initial_product_quantity)
-        this.setProductQuantity({barcode:this.scanned_product_barcode,
-                                 amount:this.initial_product_quantity})
-      }
+      if (this.product_loaded_from_cart == true) 
+        this.setProductQuantity({barcode:this.scanned_product_barcode, amount:this.initial_product_quantity})
       else
-      {
-        console.log('Attempting to remove:',this.scanned_product_barcode)
         this.removeProduct({barcode:this.scanned_product_barcode}) 
-      }
       this.hideScannedProductCard()
     },
     getProduct(barcode, businessID) {
-      console.log('line139:',typeof(barcode))
       var data = {
         "barcode": barcode,
         "businessID": businessID
@@ -132,13 +134,12 @@ export default {
       data = JSON.stringify(data);
       // TODO: handle api call failed case elegantly 
       jQuery.post(
-          "/EZBagWebapp/webapi/lookup",
+          process.env.VUE_APP_ROOT_API+"EZBagWebapp/webapi/lookup",
           data,
           function(data, status) {
             // handle json object return as string
             if (typeof(data) == "string")
               data = JSON.parse(data)
-            console.log(data)
             if (status == "success" && data.status !== "failure") {
               ref.addProduct({barcode:data.barcode,
                               name:data.name,
@@ -155,7 +156,6 @@ export default {
               description:"Not in our database",
               businessID:businessID})
             }
-            ref.show_scanned_product = true
           }
         );
     }
