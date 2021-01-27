85afc497c07afd36bb5b61099326e39d2074cae0
brendan <brendanreidy16@gmail.com>
2021-01-27 13:22:49 -0500

    added multiple queries for test to confirm

--- EZBagWebapp/src/main/java/org/tests/ProductQueryTest.java
+++ EZBagWebapp/src/main/java/org/tests/ProductQueryTest.java
@@ -31,10 +31,22 @@ public class ProductQueryTest extends JerseyTest {
 
     @Test
     public void testPost() {
-        Document insertDoc = new Document();
-        insertDoc.append("barcode", "12345678")
+        Document exampleItem = new Document();
+        exampleItem.append("barcode", "12345678")
                 .append("businessID", "1");
-        final String response = target("lookup").request().post(Entity.text(insertDoc.toJson()), String.class);
-        assertEquals(response, "{ \"_id\" : { \"$oid\" : \"6005de23132a110351032487\" }, \"barcode\" : \"12345678\", \"barcodeType\" : \"ean8\", \"businessID\" : \"1\", \"name\" : \"Example Product\", \"price\" : 9.99, \"tax\" : 0.6, \"description\" : \"Example Product is used for testing. It is not a real item!\", \"time\" : { \"$numberLong\" : \"1610997283891\" } }");
-    }
+
+        Document emptyItem = new Document();
+        emptyItem.append("barcode", "1")
+                .append("businessID", "1");
+
+        Document brokenItem = new Document();
+        emptyItem.append("asdf", "1");
+        final String response1 = target("lookup").request().post(Entity.text(exampleItem.toJson()), String.class);
+        final String response2 = target("lookup").request().post(Entity.text(emptyItem.toJson()), String.class);
+        final String response3 = target("lookup").request().post(Entity.text(brokenItem.toJson()), String.class);
+
+        assertEquals(response1, "{ \"_id\" : { \"$oid\" : \"6005de23132a110351032487\" }, \"barcode\" : \"12345678\", \"barcodeType\" : \"ean8\", \"businessID\" : \"1\", \"name\" : \"Example Product\", \"price\" : 9.99, \"tax\" : 0.6, \"description\" : \"Example Product is used for testing. It is not a real item!\", \"time\" : { \"$numberLong\" : \"1610997283891\" } }");
+        assertEquals(response2, "{\"message\":\"Product was not found in the database\",\"status\":\"failure\"}");
+        assertEquals(response3, "{\"message\":\"Barcode product lookup requires: barcode, businessID\"}");
+       }
 }
