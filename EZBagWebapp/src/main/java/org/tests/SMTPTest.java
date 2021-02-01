07a2a65c28d423ad73cbc5e2235569e2b238ce42
brendan <brendanreidy16@gmail.com>
2021-02-01 14:00:26 -0500

    added SMTP test

---
+++ EZBagWebapp/src/main/java/org/tests/SMTPTest.java
@@ -0,0 +1,21 @@
+package org.tests;
+
+import org.junit.Test;
+import org.services.EmailService;
+import org.services.Utils;
+
+import java.util.Properties;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SMTPTest {
+    public static String mediaProperties = "/usr/local/opt/EZBag/emailAndSMS.properties";
+    @Test
+    public void testSMTP()
+    {
+        Properties mediaProp = Utils.getPropertiesFile(mediaProperties);
+        EmailService.init(mediaProp);
+        int responseCode = EmailService.sendEmail("emailservicetest1234123@gmail.com", "Testing", "Test email");
+        assertEquals(responseCode, 1);
+    }
+}
