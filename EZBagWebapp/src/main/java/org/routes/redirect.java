c2b1a8b7c75a36f8852dd12a58cfac1268a91885
brendan <brendanreidy16@gmail.com>
2021-02-17 13:51:02 -0500

    Added redirect route

---
+++ EZBagWebapp/src/main/java/org/routes/redirect.java
@@ -0,0 +1,29 @@
+package org.routes;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.QueryParam;
+
+@Path("/redirect/{id}")
+public class redirect {
+    @GET
+    @Produces({MediaType.TEXT_HTML})
+    public String redirect(@PathParam("id") String ID) {
+        System.out.println(ID);
+        String returnDoc = "" +
+                "<html>\n" +
+                "<script>\n" +
+                "myStorage = window.localStorage\n" +
+                "localStorage.setItem('clearCart', 'true')\n" +
+                "localStorage.setItem('redirectID', '" + ID + "')\n" +
+                "window.location.href = '../../';\n" +
+                "</script>\n" +
+                "</html>\n";
+        return returnDoc;
+    }
+}
