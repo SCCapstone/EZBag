eb53b455e3a05ea9c780cce3f1e6cb2d78ff85d6
brendan <brendanreidy16@gmail.com>
2021-02-21 14:13:57 -0500

    Added automatic email sent to new business on sign up

--- EZBagWebapp/src/main/java/org/routes/RegisterUserRoute.java
+++ EZBagWebapp/src/main/java/org/routes/RegisterUserRoute.java
@@ -8,12 +8,16 @@ import jakarta.ws.rs.core.MediaType;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.bson.Document;
 import org.services.DatabaseService;
+import org.services.EmailService;
 import org.services.Utils;
 
 import java.util.*;
 
 @Path("/register")
 public class RegisterUserRoute {
+    static final String successfulSignupMessage = "Hello <business>,\n" +
+            "Thank you for registering with EZBag! Your application is currently under review. Upon approval, you will get full access to the EZBag merchant platform. \n\nThanks,\nEZBag Team";
+
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     public String doPost(String payload) {
@@ -32,7 +36,7 @@ public class RegisterUserRoute {
             // reject new user registration for already used email
             System.out.println(payloadObject.get("email").getAsString());
             System.out.println(DatabaseService.userExists(payloadObject.get("email").getAsString()));
-            if (DatabaseService.userExists(payloadObject.get("email").getAsString()))
+            if(DatabaseService.userExists(payloadObject.get("email").getAsString()))
             {
                 String message = "User with given email address already exists!";
                 System.out.println(message);
@@ -41,6 +45,9 @@ public class RegisterUserRoute {
                 response.addProperty("status", "failure");
                 return response.toString();
             } else {
+                String responseMessage = successfulSignupMessage.replace("<business>", payloadObject.get("businessName").getAsString());
+                EmailService.sendEmail(payloadObject.get("email").getAsString(), "Your EZBag Confirmation", responseMessage);
+
                 insertDoc.append("email", payloadObject.get("email").getAsString());
                 insertDoc.append("phone", payloadObject.get("phone").getAsString());
                 insertDoc.append("businessName", payloadObject.get("businessName").getAsString());
