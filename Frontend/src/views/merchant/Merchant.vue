a68265c6259a5c0a2fe5cda72c74cfe3fc461954
brendan <brendanreidy16@gmail.com>
2021-03-10 13:48:59 -0500

    added logout button

--- Frontend/src/views/merchant/Merchant.vue
+++ Frontend/src/views/merchant/Merchant.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <v-app-bar app elevate-on-scroll color="primary" dark>
-      <v-tabs fixed-tabs>
-          <v-tab to="store">Verification</v-tab>
-          <v-tab to="statistics">Sales</v-tab>
-      </v-tabs>
+        <v-app-bar class="tabs" app elevate-on-scroll color="primary" dark>
+          <v-tabs fixed-tabs>
+              <v-tab to="store">Verification</v-tab>
+              <v-tab to="statistics">Sales</v-tab>
+          </v-tabs>
+        </v-app-bar>
+        <v-tab class="logout" to="login"><v-icon>mdi-logout</v-icon></v-tab>
     </v-app-bar>
     <router-view/>
   </div>
@@ -17,5 +20,15 @@ export default {
 </script>
 
 <style>
-
+  .tabs{
+    width: calc(100% - 50px);
+  }
+  .logout
+  {
+    position: absolute;
+    top: 0px;
+    width: 50px;
+    height: 100%;
+    right: -22px;
+  }
 </style>
