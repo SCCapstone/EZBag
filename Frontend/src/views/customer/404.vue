ba6bb29e54c570f092310cef6de8aa6b7dfb35c5
Brendan Curran <bcurran@email.sc.edu>
2021-03-15 14:09:47 -0400

    404 vue

---
+++ Frontend/src/views/customer/404.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class = "center">
+        <v-app-bar app elevate-on-scroll color="primary" dark>
+            <p>EZBag</p>
+        </v-app-bar>
+        <div class = "center">
+            <p>404 Page Not Found</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+            
+    }
+</script>
+
+<style scoped>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        bottom: 20px;
+        left: 0;
+        right: 0;
+    }
+</style>
