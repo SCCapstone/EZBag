76f88a9be85abfb306f652b9ddd5ba3910c87eda
Brendan Curran <bcurran@email.sc.edu>
2021-03-24 13:17:35 -0400

    Made "Not Authorized" vue and added route in router

---
+++ Frontend/src/views/customer/NotAuth.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class = "center">
+            <v-app-bar app elevate-on-scroll color="primary" dark>
+                <p>EZBag</p>
+            </v-app-bar>
+                <p>Sorry, you don't have permission to access this page.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+            
+    }
+</script>
+
+<style scoped>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: static;
+        bottom: 20px;
+        left: 0;
+        right: 0;
+    }
+</style>
