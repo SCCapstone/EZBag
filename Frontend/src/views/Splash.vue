c8f2618250b115693de5d94dccf820104e5ad480
blakeedwards823 <blakeedwards823@gmail.com>
2021-04-03 15:21:41 -0400

    adds link to login screen on splash screen

--- Frontend/src/views/Splash.vue
+++ Frontend/src/views/Splash.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="splash">
-    <h2>Welcome to EZBag</h2>
+    <div id="wrapper">
+      <div class="center" id="headerText">
+            Welcome to EZBag<br><br>
+      </div>
+        <div class="center">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            Go to
+        </div>
+        <div class="center">
+            <router-link to="/login"> Merchant Login</router-link>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +27,39 @@ export default {
 </script>
 
 <style>
+  #wrapper {
+      margin-top: 13%;
+  }
+  #headerText {
+    font-size: 24px;
+  }
+  #subHeaderText {
+    font-size: 18px;
+  }
+  .v-text-field {
+    margin: 0px;
+  }
 
+  .v-select {
+    margin: 0px;
+  }
+
+  .check {
+    margin: 10px;
+  }
+
+  .center {
+    width: 100%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .v-btn {
+    border-radius: 30px;
+    height: 110%;
+  }
+
+  
 </style>
