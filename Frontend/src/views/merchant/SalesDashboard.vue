7f7a11701a4090c37d6a67cfae757d86aa459813
blakeedwards823 <blakeedwards823@gmail.com>
2021-04-26 13:57:23 -0400

    fixes bug in offset of sales dashboard

--- Frontend/src/views/merchant/SalesDashboard.vue
+++ Frontend/src/views/merchant/SalesDashboard.vue
@@ -48,8 +48,8 @@ export default {
   data() {
     return {
       showingProductSales: false,
-      HOD: ["11 PM","12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM",
-            "12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM",
+      HOD: ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM",
+            "12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM",
       ],
       DOW: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
       DOM: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
@@ -149,7 +149,7 @@ export default {
       xAxis = xAxis.reverse()
       for(i=0;i<data.length; i++){
         var time = data[i].time
-        time.setHours(time.getHours() - 1)
+        time.setHours(time.getHours())
         var day = this.HOD[time.getHours()]
         var key = xAxis.indexOf(day)
         yAxis[key] = yAxis[key] + data[i].data
